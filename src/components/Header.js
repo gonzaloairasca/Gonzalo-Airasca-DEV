5632996c3b04e2593d915cc9a58e51ef6d827e08
Gonzalo Airasca <76972966+gonzaloairasca@users.noreply.github.com>
2022-02-05 01:30:21 +0100

    nuevo commit

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+function Header(){
+    return (
+        <div className="App">
+     <h1>Gonzalo Airasca DEV</h1>
+    <ul>
+      <li>Sobre Mi</li>
+      <li>Servicios</li>
+      <li>¿Donde estamos?</li>
+      <li>Proyectos</li>
+    </ul>
+    </div>
+    )
+}
+
+
+export default Header;
---
+++ src/components/Section.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+function Section(){
+    return (<section>
+       <h3>Estas buscando crear tu sitio web? Estas en el lugar indicado!</h3>
+        <button>Quiero saber mas!</button>
+    </section>)
+}
+export default Section;
